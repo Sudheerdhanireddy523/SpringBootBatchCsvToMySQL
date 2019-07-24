acbb3174fb335fff371b1c3f34785607b7312d73
sudha <sudha@SalmaSudheer>
2019-07-24 12:44:47 +0530

    saved data successfully

---
+++ src/main/java/com/app/process/MyProcessor.java
@@ -0,0 +1,18 @@
+package com.app.process;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.app.model.Product;
+
+public class MyProcessor implements ItemProcessor<Product,Product>{
+
+	@Override
+	public Product process(Product p) throws Exception {
+		// TODO Auto-generated method stub
+		double cost=p.getCost();
+		p.setDisc(cost*3/100.0);
+		p.setGst(cost*12/100.0);
+		return p;
+	}
+
+}
